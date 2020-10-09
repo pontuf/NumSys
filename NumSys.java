c133b7de9cf17f1797cd815d2ef94610c5ae2b81
pontuf <47186351+pontuf@users.noreply.github.com>
2020-10-10 04:13:47 +0500

    Add files via upload

--- NumSys.java
+++ NumSys.java
@@ -2,18 +2,18 @@ import java.io.*;
 
 public class NumSys{
 	public static void main(String args[]) throws Exception{
-	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	    int a = Integer.parseInt(br.readLine());
 	    int b = Integer.parseInt(br.readLine());
 	    String x = br.readLine();
 	    StringBuilder res = new StringBuilder();
 	    int len = x.length();
-	   	int sum = 0;
+	   	long sum = 0;
 
 	    if(a != 10){
 	    	for(int i = 0; i < len; i++){ 
-		    	int val = 0;
+		    	long val = 0;
 		    	int temp = (int) x.charAt(i);
 		    	
 		    	if(temp > 47 && temp < 58)
@@ -28,11 +28,11 @@ public class NumSys{
 			}	
 	    }
 	    else
-			sum = Integer.parseInt(x); 
+			sum = Long.parseLong(x); 
 
 		if(b != 10){
 			while(sum > 0){
-				int val = 0;
+				long val = 0;
 		    	char temp;
 
 		    	val = sum % b;
