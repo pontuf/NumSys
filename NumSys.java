1fa56038769732ce03b3ee6699ffb3c6c261fd06
pontuf <47186351+pontuf@users.noreply.github.com>
2020-10-12 20:39:37 +0500

    Add files via upload

--- NumSys.java
+++ NumSys.java
@@ -1,54 +1,54 @@
 import java.io.*;
 
 public class NumSys{
-	public static void main(String args[]) throws Exception{
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	    int a = Integer.parseInt(br.readLine());
-	    int b = Integer.parseInt(br.readLine());
-	    String x = br.readLine();
-	    StringBuilder res = new StringBuilder();
-	    int len = x.length();
-	   	long sum = 0;
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        String x = br.readLine();
+        StringBuilder res = new StringBuilder();
+        int len = x.length();
+        long sum = 0;
 
-	    if(a != 10){
-	    	for(int i = 0; i < len; i++){ 
-		    	long val = 0;
-		    	int temp = (int) x.charAt(i);
-		    	
-		    	if(temp > 47 && temp < 58)
-		    		val += temp - 48;
-		    	else
-		    		val += temp - 87;
+        if(a != 10){
+            for(int i = 0; i < len; i++){ 
+                long val = 0;
+                int temp = (int) x.charAt(i);
+                
+                if(temp > 47 && temp < 58)
+                    val += temp - 48;
+                else
+                    val += temp - 87;
 
-		    	for(int j = len - i - 2; j >= 0 ; j--)
-		    		val *= a;
+                for(int j = len - i - 2; j >= 0 ; j--)
+                    val *= a;
 
-		    	sum += val;
-			}	
-	    }
-	    else
-			sum = Long.parseLong(x); 
+                sum += val;
+            }   
+        }
+        else
+            sum = Long.parseLong(x); 
 
-		if(b != 10){
-			while(sum > 0){
-				long val = 0;
-		    	char temp;
+        if(b != 10){
+            while(sum > 0){
+                long val = 0;
+                char temp;
 
-		    	val = sum % b;
-				sum /= b;
+                val = sum % b;
+                sum /= b;
 
-				if(val > 9)
-					val += 87;
-				else
-					val += 48;
+                if(val > 9)
+                    val += 87;
+                else
+                    val += 48;
 
-				res.insert(0, (char) val);
-			}	
-		}
-		else
-			res.append(sum);
-		
-		System.out.println(res);
-	}
+                res.insert(0, (char) val);
+            }   
+        }
+        else
+            res.append(sum);
+        
+        System.out.println(res);
+    }
 }
